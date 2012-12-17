3394e1455da9c89a59ae9e679265ea3a2ec8e31b
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-12-17 10:38:40 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtp::Application.config.secret_token = '19a854d4721a00ff7c76250583b0af17f0ea1adac1edc618f1ab45159bad9365972931af5458bb19f5a73f742e1524e12ffab3a38275e0075563164c3e8e1691'
