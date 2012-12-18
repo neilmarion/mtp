6d60f26596b5bba2461a4d53a2807337ed1132ff
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-12-18 11:27:10 +0800

    some progress

--- spec/views/people/index.html.erb_spec.rb
+++ spec/views/people/index.html.erb_spec.rb
@@ -21,15 +21,10 @@ describe "people/index" do
       )
     ])
   end
-
-  it "renders a list of people" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Middle Name".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "Birth Date".to_s, :count => 2
+  
+  it "renders top menu" do
+    render :template => "people/index", :layout => "layouts/application"
+  
+    assert_select "div", :text => "People"
   end
 end
