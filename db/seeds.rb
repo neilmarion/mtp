bb960fc1ce1d2310f9b0efd9faf2f05db32126f4
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2012-12-25 11:19:13 +0800

    Added seeds for Offices.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Office.all.empty?
+  Office.create(name: 'Deaconship', organization_id: 0)
+  Office.create(name: 'Choir', organization_id: 0)
+  Office.create(name: 'Finance', organization_id: 0)
+  Office.create(name: 'Secretariat', organization_id: 0)
+  Office.create(name: 'SCAN', organization_id: 0)
+  Office.create(name: 'PNK', organization_id: 0)
+  Office.create(name: 'Buklod', organization_id: 0)
+  Office.create(name: 'Kadiwa', organization_id: 0)
+  Office.create(name: 'Binhi', organization_id: 0)
+end
