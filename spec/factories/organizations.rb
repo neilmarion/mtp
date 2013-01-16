984acf5f4f8fb146ca096713bf968d1789a955a1
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-01-16 14:18:47 +0800

    starting off capybara selenium tests

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :organization do
     sequence(:name) {|n| "Organization_#{n}" }
+    type_of_children "children"
   end
 end
--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
-describe "People" do
+describe "People", :js => :true do
+  before(:each) do
+    #@office = FactoryGirl.create(:office)
+    @organization = FactoryGirl.create(:organization)
+    @child = FactoryGirl.create(:organization, parent: @organization)
+  end
+  
   describe "GET /people" do
-    pending
+    it "displays people" do
+      person = FactoryGirl.create(:person)
+      visit people_path
+      page.should have_content(person.first_name)
+    end
   end
 end
