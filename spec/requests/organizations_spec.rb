e881de9b933c7b86671c0e35421f9d76e72fd792
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-01-23 17:31:58 +0800

    Added Ajax tests for organizations.

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -7,22 +7,45 @@ describe "Organizations", :js => :true do
     @org_3 = FactoryGirl.create(:organization, parent: @org_1)
     @org_4 = FactoryGirl.create(:organization, parent: @org_2)
     @org_5 = FactoryGirl.create(:organization, parent: @org_2)
+    
+    visit organizations_path
   end
 
   describe "GET /organizations" do
-    before(:each) do
-      visit organizations_path
-    end
-  
     it "displays 2nd-level organizations at redirect" do
       page.should have_content(@org_2.name)
       page.should have_content(@org_3.name)
     end
     
-    it "sub-orgs of an org at click" do
+    it "displays sub-orgs of an org at click" do
       click_link @org_2.name
       page.should have_content(@org_4.name)
       page.should have_content(@org_5.name)
+    end
+  end
+  
+  describe "POST /organizations" do
+    it "adds an organization" do
+      click_link "+ Add new #{@org_1.type_of_children}"
+      fill_in "Name", :with => "New Org"
+      fill_in "Type of children", :with => "Children"
+      click_button "Save and close"
+      page.should have_content "New Org"
+    end
+    
+    it "adds an organization (Save and Add Another)" do
+      click_link "+ Add new #{@org_1.type_of_children}"
+      fill_in "Name", :with => "New Org"
+      fill_in "Type of children", :with => "Children"
+      click_button "Save and add another"
+      page.should have_content "New Org"
+      
+      page.should have_content "Add a Sub-Organization"
+      
+      fill_in "Name", :with => "New Org 2"
+      fill_in "Type of children", :with => "Children"
+      click_button "Save and add another"
+      page.should have_content "New Org 2"
     end
   end
 end
