f8e45a2661f749a34659f1270bc4a2fcd78dafc1
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-01-23 17:01:42 +0800

    Some tests for organizations

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
-describe "Organizations" do
+describe "Organizations", :js => :true do
+  before(:each) do
+    @org_1 = FactoryGirl.create(:organization)
+    @org_2 = FactoryGirl.create(:organization, parent: @org_1)
+    @org_3 = FactoryGirl.create(:organization, parent: @org_1)
+    @org_4 = FactoryGirl.create(:organization, parent: @org_2)
+    @org_5 = FactoryGirl.create(:organization, parent: @org_2)
+  end
+
   describe "GET /organizations" do
-    pending
+    before(:each) do
+      visit organizations_path
+    end
+  
+    it "displays 2nd-level organizations at redirect" do
+      page.should have_content(@org_2.name)
+      page.should have_content(@org_3.name)
+    end
+    
+    it "sub-orgs of an org at click" do
+      click_link @org_2.name
+      page.should have_content(@org_4.name)
+      page.should have_content(@org_5.name)
+    end
   end
 end
