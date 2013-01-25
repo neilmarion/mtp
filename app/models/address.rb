5ecd78ea6fef8ec141a07fb690ab1c76a7da607c
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-01-25 20:16:29 +0800

    Made reverse-geocoding work. But still can't access the address.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,13 @@
 class Address < ActiveRecord::Base
-  attr_accessible :address
+  attr_accessible :address, :latitude, :longitude
   
   belongs_to :person
+  
+  geocoded_by :address_for_geocode
+  reverse_geocoded_by :latitude, :longitude, :address => :address
+  after_create :reverse_geocode  # auto-fetch address
+  
+  def get_address
+    address
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130125065653) do
+ActiveRecord::Schema.define(:version => 20130125115409) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address"
     t.integer  "person_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "cfos", :force => true do |t|
