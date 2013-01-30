082530d2cef621afc514100a32fe20d2e50d4d79
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-01-31 01:55:16 +0800

    Added helper that renders the organizations dropdown.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def new_render_org_tree(org_tree)
+    ret = ''
+    org_tree.each do |org_id, children|
+      org = Organization.find(org_id)
+      next unless org # Just in case there's some bad data
+      ret += render(partial: 'application/new_org', locals: {org: org, children: children})
+    end
+    ret.html_safe
+  end
 end
