264d4df5820ef762e587fafccbb1884e377150c7
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-02-01 20:28:49 +0800

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   def org_tree_node(o = nil)
     orgs = {}
     @org_ids ||= {}
-    (o ? o.children : Organization.find(organization_id).children).order('name').each do |org|
+    (o ? o.children : Organization.find(Organization.find(organization_id).parent).children).order('name').each do |org|
       # collect roles associated with each org
       @org_ids[org.id] ||= {}
       orgs[org.id] = org_tree_node(org)
