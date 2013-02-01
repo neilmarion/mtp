3ec622e8f3ec29e3e51dd3a6ec1c7693534b798b
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-02-01 09:05:42 +0800

    Some modifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
   def new_render_org_tree(org_tree)
     return if org_tree.blank?
-    puts org_tree.inspect
+    #puts org_tree.inspect
     ret = ''
     org_tree.each do |org_id, children|
       org = Organization.find(org_id)
+      puts org.name.inspect
       next unless org # Just in case there's some bad data
       ret += render(partial: 'application/new_org', locals: {org: org, children: children})
     end
